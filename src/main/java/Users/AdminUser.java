d89c3d09f130c9dbad91bd658586805890842954
xfarkasp <xfarkasp@stuba.sk>
2023-04-02 21:26:37 +0200

    -packaging

--- src/main/java/Users/AdminUser.java
+++
@@ -1,9 +0,0 @@
-package Users;
-
-import java.util.ArrayList;
-
-public class AdminUser extends DefaultUser{
-    public AdminUser(String firstName, String lastName, String email, String password, int age, ArrayList<String> hobbies, ArrayList<String> foods, ArrayList<String> languages, String originCountry) {
-        super(firstName, lastName, email, password, age, hobbies, foods, languages, originCountry);
-    }
-}
